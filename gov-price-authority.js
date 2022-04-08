30d5abcb1ee95e8662fccf1123bce44362516047
Michael FIG <michael+github@fig.org>
2022-04-07 23:45:06 -0600

    chore: attach priceAuthority governance

---
+++ gov-price-authority.js
@@ -0,0 +1,14 @@
+const startPriceAuthority = async ({
+  consume: { loadVat },
+  produce,
+}) => {
+  const vats = { priceAuthority: E(loadVat)('priceAuthority') };
+  const { priceAuthority, adminFacet } = await E(
+    vats.priceAuthority,
+  ).makePriceAuthorityRegistry();
+
+  produce.priceAuthorityVat.resolve(vats.priceAuthority);
+  produce.priceAuthority.resolve(priceAuthority);
+  produce.priceAuthorityAdmin.resolve(adminFacet);
+};
+harden(startPriceAuthority);
